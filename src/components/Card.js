463c92c9f2043393c78354529a2f132960dec8a5
PotatOceanus <39735195+PotatOceanus@users.noreply.github.com>
2022-04-21 16:32:44 +1000

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = ({ name, email, id }) => {
+    return (
+        // <h1>RoboFriends</h1>
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt = 'rebots' src={`https://robohash.org/${id}?100*100`} />
+            <div>
+                <h1>{name}</h1>
+                <p>{email}</p>    
+            </div>    
+        </div>
+    );
+}
+
+export default Card;
